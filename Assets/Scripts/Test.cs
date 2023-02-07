a7c8cf38452ae99806a1dbb7bee9669e6acc3efd
Evgen Konyshok <lunevgeniy@gmail.com>
2023-02-07 20:31:39 +0200

    Add assets with graphics

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    private int number = 5;
+    private double doubleNumber = 314.5235235f;
+    private float floatNumber = 314.5235235f;
+    private string stroka = "Hello";
+    private bool flag = true;
+    private bool flag2 = false;
+    
+    void Start()
+    {
+        // print(number);
+        // print(stroka);
+        // print(flag);
+        // print(flag2);
+        // print(floatNumber);
+        // print(doubleNumber);
+
+        if (flag)
+        {
+            print("Flag is true");
+        }
+        else
+        {
+            print("Flag is not true");
+        }
+
+        if (number > 10)
+        {
+            print("number > 10");
+        }
+        else if (number == 10)
+        {
+            print("number == 10");
+        }
+        else
+        {
+            print("number < 10");
+        }
+        
+        print(this.gameObject.name);
+
+        gameObject.name = "NewName";
+        
+        print(this.gameObject.name);
+    }
+}
